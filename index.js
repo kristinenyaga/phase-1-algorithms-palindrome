43c15085e1fd9358860f9b1c691abe1ee44473a3
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-25 03:23:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // how to check whether a number is palindrome or not:
+    // Steps
+      // 1.Convert string to array
+      word=word.toLowerCase()
+      const stringToArray=word.split('')
+      // 2.Reverse array values
+      const reversedArray=stringToArray.reverse()
+      // 3.convert reversed array back to string
+      const stringValue=reversedArray.join('')
+      // 2.Wtite a condition that checks whether the reversed string is equal to word
+      if(word === stringValue){
+        return word === stringValue
+      }
+      
+      return false
+      
 }
+
 
 /* 
   Add your pseudocode here
+
+  
+  A  word is said to be a palindrome if the word and the reverse of the word are the same
+  Initialize a function and pass in a parameter(word)
+  convert the word  to lowercase
+  convert the  word to an array(stringToArray) using split()
+  reverse the  letters  in stringToarray using reverse
+  convert the reversed array to a string using join()
+  compare the word(parameter) with the reversed string to see whether they match 
+  return true if they do and false if otherwise
+
 */
 
 /*
